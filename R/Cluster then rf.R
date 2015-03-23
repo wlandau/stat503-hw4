438ccfa76780a4406879cedc5bb0e15af4a10a7c
fallingbella <fallingbella@gmail.com>
2015-03-22 21:50:09 -0500

    Test two.

--- R/Cluster then rf.R
+++ R/Cluster then rf.R
@@ -122,6 +122,31 @@ ggparcoord(df14, columns=3:17, groupColumn=19,scale="globalminmax", boxplot=TRUE
            showPoints=FALSE, alphaLines=0.05) + 
   xlab("") + ylab("PctCorrect") + theme_bw()
 
+
+df.ex14 <- df14 %>%
+  group_by(cl) %>%
+  mutate(average.cl=mean(average))%>%
+  mutate(st.cl=sd(average))%>%
+  mutate(ave.1=mean(Ch1))%>%
+  mutate(ave.2=mean(Ch2))%>%
+  mutate(ave.3=mean(Ch3))%>%
+  mutate(ave.4=mean(Ch4))%>%
+  mutate(ave.5=mean(Ch5))%>%
+  mutate(ave.6=mean(Ch6))%>%
+  mutate(ave.78=mean(Ch7.8))%>%
+  mutate(ave.9=mean(Ch9))%>%
+  mutate(ave.10=mean(Ch10))%>%
+  mutate(ave.11=mean(Ch11))%>%
+  
+  mutate(ave.151=mean(Ch15I))%>%
+  mutate(ave.152=mean(Ch15II))%>%
+  mutate(ave.16=mean(Ch16))%>%
+  mutate(ave.17=mean(Ch17))%>%
+ 
+  mutate(ave.20=mean(Ch20))%>%
+  select(19:36)%>%
+  distinct
+
 ##Random forest, 14spring does not have Ch12, Ch18, Ch19
 train<- df[,c(3:12,14:17,20,22)]
 test<-df14[,c(3:17,19)]
